f3a9a705ce8fc4162ba381eeba025cf24cd94aeb
Matias Vaz <matiasfreitasguimaraes@gmail.com>
2022-04-26 17:21:37 +0100

    makefile for q2, q3

---
+++ q2/addmx.h
@@ -0,0 +1,8 @@
+//
+// Created by Matias Freitas Guimarães on 26/04/2022.
+//
+
+#ifndef SOWORK_ADDMX_H
+#define SOWORK_ADDMX_H
+
+#endif //SOWORK_ADDMX_H
---
+++ q3/cypher.h
@@ -0,0 +1,8 @@
+//
+// Created by Matias Freitas Guimarães on 26/04/2022.
+//
+
+#ifndef SOWORK_CYPHER_H
+#define SOWORK_CYPHER_H
+
+#endif //SOWORK_CYPHER_H
