3d3e9cc6e71086f85cb02ac5252a9e21751b1c29
diogofonte <diogo.fonte2000@gmail.com>
2022-04-26 17:35:13 +0100

    Update phrases.c

--- q1/phrases.c
+++ q1/phrases.c
@@ -1,36 +1,58 @@
 #include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/uio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 #define BUFFER_SIZE 1024
 
-int next_block_size(int count, int buffer_size) {
-    return (count >= buffer_size)? buffer_size: count % buffer_size;
+char[] readFile(FILE* file){
+    if(input == NULL){
+        printf("Não foi possível abrir o ficheiro");
+        return -1;
+    }
+    char[BUFFER_SIZE][BUFFER_SIZE] string = {};
+    int i = 0, j = 0;
+    char c = fgetc(input);
+    while(c != EOF){
+        j = 0;
+        while(c != '!' && c != '?' && c != '.'){
+            string[i][j] = c;
+            //printf("%c", c);
+            c = fgetc(input);
+            j++;
+        }
+        // a partir daqui temos pontos de terminação de frases
+        // não sei se cobre a parte das reticências mas penso que sim
+        string[i][j] = c;
+        i++;
+    }
 }
 
 int main(int argc, char* argv[]) {
     /* check if there is no argument */
-    if (argc == 1) {
+    if(argc == 1) {
       printf("usage: phrases [-l] file\n");
       return EXIT_FAILURE;
+    } else if(argc == 2){
+        FILE* input = fopen(argv[1], "r");
+
+        char[BUFFER_SIZE][BUFFER_SIZE] string = readFile(input);
+
+        fclose(input);
+    } else if(argc == 3) {
+        FILE* input = fopen(argv[2], "r");
+
+        char[BUFFER_SIZE][BUFFER_SIZE] string = readFile(input);
+
+        fclose(input);
+
+        for(int i = 0 ; ?? ; i++){
+            print("[%d] ",i+1);
+            for(int j = 0 ; string[i][j] != '\0' ; j++){
+                printf("%c", string[i][j]);
+            }
+            printf("\n");
+        }
     }
 
-    if(argc == 2){
-        FILE* input = fopen(argv[1], "r");
-        if(input == NULL){
-            printf("Não foi possível abrir o ficheiro");
-            return -1;
-        }
-        char c = fgetc(input);
-        while(c != EOF){
-            printf("%c", c);
-            c = fgetc(input);
-        }
-        fclose(input);
-    }
 
     return EXIT_SUCCESS;
 }
