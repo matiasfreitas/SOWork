800e36349045043125ded5440ac42822645ae75e
Matias Vaz <matiasfreitasguimaraes@gmail.com>
2022-05-17 18:27:28 +0100

    q1 and q2 changes

--- q1/phrases.c
+++ q1/phrases.c
@@ -4,6 +4,28 @@
 #include <ctype.h>
 #define BUFFER_SIZE 1024
 
+void readFile(FILE* input, char string[]){
+    char character = fgetc(input);
+    int i = 0;
+    while(character != '!' && character != '?' && character != '.'){
+        string[i] = character;
+        i++;
+        character = fgetc(input);
+    }
+    while(character == '!' || character == '?' || character == '.') {
+        string[i] = character;
+        character = fgetc(input);
+        i++;
+        if (isspace(character)){
+            string[i] = character;
+            string[i+1] = '\0';
+            break;
+        }
+    }
+    return;
+}
+
+
 int main(int argc, char* argv[]) {
     FILE* input;
     if (argc == 2) {
@@ -20,55 +42,24 @@ int main(int argc, char* argv[]) {
         printf("Unable to open the file\n");
         return -1;
     }
-    char string[BUFFER_SIZE][BUFFER_SIZE];
-    int linenum = 0, j = 0;
-    char lastchar;
-
-    do
-    {
-        char c = fgetc(input);
-        if (feof(input)){
-            if(lastchar != '!' && lastchar != '?' && lastchar != '.'){
-                linenum++;
-            }
-            break;
-        }
-        string[linenum][j] = c;
-        if (!isspace(c)){
-            lastchar = c;
-        }
-        if(c == '!' || c == '?' || c == '.'){
-            FILE* temp = input;
-            int ret = 0;
-            while((c = fgetc(input)) == '.'){
-                ret = 1;
-                j++;
-                string[linenum][j] = c;
-            }
-            if(ret== 0){
-                input = temp;
-            }
-            string[linenum][j+1] = '\0';
-            linenum++;
-            j=0;
-            continue;
-        }
-        j++;
-    }  while(1);
-    fclose(input);
-    if (argc == 2) {
-        printf("%d\n",linenum);
-    }
-    else{
-        for(int i = 0 ; i < linenum ; i++){
-            printf("[%d] ",i+1);
+    char string[BUFFER_SIZE];
+    int lineNum = 0;
+    while(!feof(input)){
+        readFile(input, string);
+        if (strcmp(argv[1],"-l") == 0){
+            printf("[%d] ",lineNum+1);
             int j = 0;
-            while(string[i][j] != '\0'){
-                printf("%c",string[i][j]);
+            while(string[j] != '\0'){
+                printf("%c",string[j]);
                 j++;
             }
             printf("\n");
         }
+        lineNum ++;
     }
-    return EXIT_SUCCESS;
+    fclose(input);
+    if (argc == 2) {
+        printf("%d\n",lineNum);
+    }
+    return 0;
 }
--- q2/addmx.c
+++ q2/addmx.c
@@ -22,7 +22,6 @@ int* addMatrix(int nLines, int nColumns, int* matrix1, int* matrix2, int offsetM
         printf("Result Matrix Failed\n");
         exit(1);
     }
-
     pid_t pids[nColumns];
     int n = nColumns;
     for (int j = 0; j < nColumns; ++j) {
@@ -79,6 +78,35 @@ int* readMatrix(int nLines, int nColumns,int* offsetMatrix, char* matrixContent)
     return matrixResult;
 }
 
+char *openFile(char *argv[], int index){
+    const char *filepathMatrix1 = argv[index];
+    int fileMatrix1 = open(filepathMatrix1, O_RDWR);
+    if(fileMatrix1 < 0){
+        printf("\n\"%s \" could not open\n",
+               filepathMatrix1);
+        exit(1);
+    }
+
+    struct stat statbufMatrix1;
+    int errMatrix1 = fstat(fileMatrix1, &statbufMatrix1);
+    if(errMatrix1 < 0){
+        printf("\n\"%s \" could not open\n",
+               filepathMatrix1);
+        exit(2);
+    }
+    char *Matrix1Content = mmap(NULL,statbufMatrix1.st_size,
+                                PROT_READ,
+                                MAP_SHARED,
+                                fileMatrix1,0);
+    if(Matrix1Content == MAP_FAILED){
+        printf("Mapping Matrix 1 Content Failed\n");
+        exit(1);
+    }
+    return Matrix1Content;
+}
+
+
+
 int main(int argc, char *argv[]){
     if(argc == 1){
         printf("usage: addmx file1 file2\n");
@@ -88,64 +116,21 @@ int main(int argc, char *argv[]){
         printf("File path not mentioned\n");
         exit(0);
     }
-    const char *filepathMatrix1 = argv[1];
-    int fileMatrix1 = open(filepathMatrix1, O_RDWR);
-    if(fileMatrix1 < 0){
-        printf("\n\"%s \" could not open\n",
-               filepathMatrix1);
-        exit(1);
-    }
-    const char *filepathMatrix2 = argv[2];
-    int fileMatrix2 = open(filepathMatrix2, O_RDWR);
-    if(fileMatrix2 < 0){
-        printf("\n\"%s \" could not open\n",
-               filepathMatrix2);
-        exit(1);
-    }
-    struct stat statbufMatrix1;
-    int errMatrix1 = fstat(fileMatrix1, &statbufMatrix1);
-    if(errMatrix1 < 0){
-        printf("\n\"%s \" could not open\n",
-               filepathMatrix1);
-        exit(2);
-    }
-    struct stat statbufMatrix2;
-    int errMatrix2 = fstat(fileMatrix1, &statbufMatrix2);
-    if(errMatrix2 < 0){
-        printf("\n\"%s \" could not open\n",
-               filepathMatrix2);
-        exit(2);
-    }
 
-    char *Matrix1Content = mmap(NULL,statbufMatrix1.st_size,
-                                PROT_READ,
-                                MAP_SHARED,
-                                fileMatrix1,0);
-    if(Matrix1Content == MAP_FAILED){
-        printf("Mapping Matrix 1 Content Failed\n");
-        exit(1);
-    }
-
-    char *Matrix2Content = mmap(NULL,statbufMatrix2.st_size,
-                                PROT_READ,
-                                MAP_SHARED,
-                                fileMatrix2,0);
-    if(Matrix2Content == MAP_FAILED){
-        printf("Mapping Matrix 2 Content Failed\n");
-        exit(1);
-    }
-
+    char *Matrix1Content = openFile(argv, 1);
     int matrix1Columns, matrix1Rows;
     int offsetMatrix1 = 0;
     matrix1Rows = readNumber(Matrix1Content, &offsetMatrix1, 'x');
     matrix1Columns = readNumber(Matrix1Content, &offsetMatrix1, '\n');
     int *Matrix1 = readMatrix(matrix1Rows, matrix1Columns, &offsetMatrix1, Matrix1Content);
 
+    char *Matrix2Content = openFile(argv, 2);
     int matrix2Columns, matrix2Rows;
     int offsetMatrix2 = 0;
     matrix2Rows = readNumber(Matrix2Content, &offsetMatrix2, 'x');
     matrix2Columns = readNumber(Matrix2Content, &offsetMatrix2, '\n');
     int *Matrix2 = readMatrix(matrix2Rows, matrix2Columns, &offsetMatrix2, Matrix2Content);
+
     if(matrix1Rows == matrix2Rows && matrix1Columns == matrix2Columns){
         int* matrixResult = addMatrix(matrix1Rows, matrix1Columns, Matrix1, Matrix2, offsetMatrix1, offsetMatrix2);
         for (int i = 0; i < matrix1Rows; ++i) {
@@ -154,6 +139,10 @@ int main(int argc, char *argv[]){
             }
             printf("\n");
         }
+    }
+    else{
+        printf("\nMatrix with different sizes.\n");
+        exit(1);
     }
     return 0;
 }
